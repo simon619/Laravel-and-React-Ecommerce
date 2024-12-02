badbf8cdfd94a32af0e8fd54a9ec01f73de16459
simon619 <simonavi619@gmail.com>
2024-12-02 23:10:04 +0600

    master

--- loops.php
+++ loops.php
@@ -3,12 +3,7 @@
     for ($i = 0; $i < 10; $i++) {
 
         echo $i . '</br>';
-<<<<<<< HEAD
-        echo 'Test 3 Branch';
-=======
-        echo 'master';
-
->>>>>>> master
+        echo "master";
 
     }
 
